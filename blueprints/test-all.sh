27e4c51ced08bd1c847af79e24fe92a233ed2481
Carlos Rodriguez Lopez <it.carlosrodlop@gmail.com>
2023-12-13 11:27:38 +0100

    Moving Docker and Test inside the Blueprint folder

--- blueprints/test-all.sh
+++ blueprints/test-all.sh
@@ -12,13 +12,12 @@ HERE="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export TF_LOG=DEBUG
 
 declare -a bluePrints=(
-  "getting-started/v4"
-  "getting-started/v5"
+  "getting-started"
 )
 
 test () {
   printf "\033[36mRunning Smoke Test for CloudBees CI Blueprint %s...\033[0m\n\n" "$1"
-  export ROOT=$1 
+  export ROOT=$1
   export TF_LOG_PATH="$HERE/$ROOT/terraform.log"
   cd "$HERE"/.. && make tfDeploy
   cd "$HERE"/.. && make validate
