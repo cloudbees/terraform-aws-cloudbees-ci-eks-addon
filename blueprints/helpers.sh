1b5329752b7e94173dce8c81699c35757672c9ad
Carlos Rodriguez Lopez <it.carlosrodlop@gmail.com>
2024-01-19 18:35:42 +0100

    Chore: Testing Helpers for BP 02

--- blueprints/helpers.sh
+++ blueprints/helpers.sh
@@ -6,8 +6,6 @@ SCRIPTDIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 MSG_INFO="\033[36m[INFO] %s\033[0m\n"
 
-export TF_LOG=DEBUG
-
 test () {
   local ROOT=$1
   export TF_LOG_PATH="$SCRIPTDIR/$ROOT/terraform.log"
@@ -37,6 +35,7 @@ get-tf-output () {
 rules-general () {
   local ROOT=$1
   eval "$(get-tf-output "$ROOT" kubeconfig_export)"
+  until [ "$(eval "$(get-tf-output "$ROOT" cbci_oc_pod)" | awk '{ print $3 }' | grep -v STATUS | grep -v -c Running)" == 0 ]; do sleep 10 && echo "Waiting for Operation Center Pod to get ready..."; done ;\
   until eval "$(get-tf-output "$ROOT" cbci_oc_pod)"; do sleep 10 && echo "Waiting for Operation Center Pod to get ready..."; done ;\
     printf "$MSG_INFO" "OC Pod is Ready."
   until eval "$(get-tf-output "$ROOT" cbci_liveness_probe_int)"; do sleep 10 && echo "Waiting for Operation Center Service to pass Health Check from inside the cluster..."; done
@@ -56,6 +55,18 @@ rules-bp01 () {
 
 rules-bp02 () {
   local ROOT="02-at-scale"
+  eval "$(get-tf-output "$ROOT" kubeconfig_export)"
   GENERAL_PASS=$(eval "$(get-tf-output "$ROOT" cbci_general_password)"); \
     printf "$MSG_INFO" "General Password all users: $GENERAL_PASS."
+  until [ "$(eval "$(get-tf-output "$ROOT" cbci_controllers_pods)" | awk '{ print $3 }' | grep -v STATUS | grep -v -c Running)" == 0 ]; do sleep 10 && echo "Waiting for Controllers Pod to get into Ready State..."; done ;\
+    printf "$MSG_INFO" "Controllers Pods are Ready..."
+  until eval "$(get-tf-output "$ROOT" cbci_controllers_pods)"; do sleep 10 && echo "Waiting for Team C HPA to get Ready..."; done ;\
+    printf "$MSG_INFO" "Team C HPA is Ready..."
+  eval "$(get-tf-output "$ROOT" velero_backup_schedule_team_a)" && eval "$(get-tf-output "$ROOT" velero_backup_on_demand_team_a)" > "/tmp/backup.txt" && \
+		cat "/tmp/backup.txt" | grep "Backup completed with status: Completed" && \
+		printf "$MSG_INFO" "Velero backups are working"
+  until eval "$(get-tf-output "$ROOT" prometheus_active_targets)" | jq '.data.activeTargets[] | select(.labels.container=="jenkins" or .labels.job=="cjoc") | {job: .labels.job, instance: .labels.instance, status: .health}'; do sleep 10 && echo "Waiting for CloudBees CI Prometheus Targets..."; done ;\
+    printf "$MSG_INFO" "CloudBees CI Targets are loaded in Prometheus..."
+  until eval "$(get-tf-output "$ROOT" aws_fluentbit_logstreams)" | grep logStreamName | grep jenkins; do sleep 10 && echo "Waiting for CloudBees CI Log streams in CloudWatch..."; done ;\
+    printf "$MSG_INFO" "CloudBees CI Log Streams are already in Cloud Watch..."
 }
