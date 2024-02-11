c88b4f5c6907902edcc1966648a49c56d9562436
Carlos Rodriguez Lopez <it.carlosrodlop@gmail.com>
2024-02-11 21:50:16 +0100

    Chore: Refactor validation

--- blueprints/helpers.sh
+++ blueprints/helpers.sh
@@ -62,6 +62,7 @@ tf-output () {
 #https://aws-ia.github.io/terraform-aws-eks-blueprints/getting-started/#deploy
 tf-deploy () {
   local root=$1
+  export TF_LOG_PATH="$SCRIPTDIR/$root/terraform.log"
   retry 2 "terraform -chdir=$SCRIPTDIR/$root init"
   retry 2 "terraform -chdir=$SCRIPTDIR/$root apply -target=module.vpc -auto-approve"
   retry 2 "terraform -chdir=$SCRIPTDIR/$root apply -target=module.eks -auto-approve"
@@ -72,14 +73,15 @@ tf-deploy () {
 #https://aws-ia.github.io/terraform-aws-eks-blueprints/getting-started/#destroy
 tf-destroy () {
   local root=$1
-  retry 2 "terraform -chdir=$SCRIPTDIR/$root destroy -target=module.eks_blueprints_addon_cbci -auto-approve"
-  retry 2 "terraform -chdir=$SCRIPTDIR/$root destroy -target=module.eks_blueprints_addons -auto-approve"
-  retry 2 "terraform -chdir=$SCRIPTDIR/$root destroy -target=module.eks -auto-approve"
-  retry 2 "terraform -chdir=$SCRIPTDIR/$root destroy -auto-approve"
+  export TF_LOG_PATH="$SCRIPTDIR/$root/terraform.log"
+  retry 3 "terraform -chdir=$SCRIPTDIR/$root destroy -target=module.eks_blueprints_addon_cbci -auto-approve"
+  retry 3 "terraform -chdir=$SCRIPTDIR/$root destroy -target=module.eks_blueprints_addons -auto-approve"
+  retry 3 "terraform -chdir=$SCRIPTDIR/$root destroy -target=module.eks -auto-approve"
+  retry 3 "terraform -chdir=$SCRIPTDIR/$root destroy -auto-approve"
   rm -f "$SCRIPTDIR/$root/terraform.output"
 }
 
-probes-common () {
+probes () {
   local root=$1
   eval "$(tf-output "$root" kubeconfig_export)"
   until [ "$(eval "$(tf-output "$root" cbci_oc_pod)" | awk '{ print $3 }' | grep -v STATUS | grep -v -c Running)" == 0 ]; do sleep 10 && echo "Waiting for Operation Center Pod to get ready..."; done ;\
@@ -91,31 +93,30 @@ probes-common () {
   OC_URL=$(tf-output "$root" cbci_oc_url)
   until eval "$(tf-output "$root" cbci_liveness_probe_ext)"; do sleep $RETRY_SECONDS && echo "Waiting for Operation Center Service to pass Health Check from outside the clustery..."; done ;\
     INFO "Operation Center Service passed Health Check outside the cluster. It is available at $OC_URL."
-}
-
-probes-bp01 () {
-  local root="01-getting-started"
-  eval "$(tf-output "$root" kubeconfig_export)"
-  INITIAL_PASS=$(eval "$(tf-output "$root" cbci_initial_admin_password)"); \
-    INFO "Initial Admin Password: $INITIAL_PASS."
-}
-
-probes-bp02 () {
-  local root="02-at-scale"
-  eval "$(tf-output "$root" kubeconfig_export)"
-  GENERAL_PASS=$(eval "$(tf-output "$root" cbci_general_password)"); \
+  if [ "$root" == "01-getting-started" ]; then
+    INITIAL_PASS=$(eval "$(tf-output "$root" cbci_initial_admin_password)"); \
+      INFO "Initial Admin Password: $INITIAL_PASS."
+  fi
+  if [ "$root" == "02-at-scale" ]; then
+    GENERAL_PASS=$(eval "$(tf-output "$root" cbci_general_password)"); \
     INFO "General Password all users: $GENERAL_PASS."
-  until [ "$(eval "$(tf-output "$root" cbci_controllers_pods)" | awk '{ print $3 }' | grep -v STATUS | grep -v -c Running)" == 0 ]; do sleep $RETRY_SECONDS && echo "Waiting for Controllers Pod to get into Ready State..."; done ;\
-    eval "$(tf-output "$root" cbci_controllers_pods)" && INFO "All Controllers Pods are Ready..."
-  until eval "$(tf-output "$root" cbci_controller_c_hpa)"; do sleep $RETRY_SECONDS && echo "Waiting for Team C HPA to get Ready..."; done ;\
-    INFO "Team C HPA is Ready."
-  eval "$(tf-output "$root" velero_backup_schedule_team_a)" && eval "$(tf-output "$root" velero_backup_on_demand_team_a)" > "/tmp/backup.txt" && \
-		grep "Backup completed with status: Completed" "/tmp/backup.txt" && \
-		INFO "Velero backups are working"
-  until eval "$(tf-output "$root" prometheus_active_targets)" | jq '.data.activeTargets[] | select(.labels.container=="jenkins" or .labels.job=="cjoc") | {job: .labels.job, instance: .labels.instance, status: .health}'; do sleep $RETRY_SECONDS && echo "Waiting for CloudBees CI Prometheus Targets..."; done ;\
-    INFO "CloudBees CI Targets are loaded in Prometheus."
-  until eval "$(tf-output "$root" aws_logstreams_fluentbit)" | jq '.[] '; do sleep $RETRY_SECONDS && echo "Waiting for CloudBees CI Log streams in CloudWatch..."; done ;\
-    INFO "CloudBees CI Log Streams are already in Cloud Watch."
+    until [ "$(eval "$(tf-output "$root" cbci_controllers_pods)" | awk '{ print $3 }' | grep -v STATUS | grep -v -c Running)" == 0 ]; do sleep $RETRY_SECONDS && echo "Waiting for Controllers Pod to get into Ready State..."; done ;\
+      eval "$(tf-output "$root" cbci_controllers_pods)" && INFO "All Controllers Pods are Ready..."
+    until eval "$(tf-output "$root" cbci_controller_c_hpa)"; do sleep $RETRY_SECONDS && echo "Waiting for Team C HPA to get Ready..."; done ;\
+      INFO "Team C HPA is Ready."
+    eval "$(tf-output "$root" cbci_oc_export_admin_crumb)" && eval "$(tf-output "$root" cbci_oc_export_admin_api_token)" && \
+      if [ -n "$CBCI_ADMIN_TOKEN" ]; then INFO "Admin Token: $CBCI_ADMIN_TOKEN"; else ERROR "Problem while getting Admin Token"; fi
+    eval "$(tf-output "$root" cbci_controller_b_hibernation_post_queue_ws_cache)" > /tmp/ws-cache-build-trigger && \
+      grep "HTTP/2 201" /tmp/ws-cache-build-trigger && \
+      INFO "Hibernation Post Queue WS Cache is working."
+    eval "$(tf-output "$root" velero_backup_schedule_team_a)" && eval "$(tf-output "$root" velero_backup_on_demand_team_a)" > "/tmp/backup.txt" && \
+      grep "Backup completed with status: Completed" "/tmp/backup.txt" && \
+      INFO "Velero backups are working"
+    until eval "$(tf-output "$root" prometheus_active_targets)" | jq '.data.activeTargets[] | select(.labels.container=="jenkins" or .labels.job=="cjoc") | {job: .labels.job, instance: .labels.instance, status: .health}'; do sleep $RETRY_SECONDS && echo "Waiting for CloudBees CI Prometheus Targets..."; done ;\
+      INFO "CloudBees CI Targets are loaded in Prometheus."
+    until eval "$(tf-output "$root" aws_logstreams_fluentbit)" | jq '.[] '; do sleep $RETRY_SECONDS && echo "Waiting for CloudBees CI Log streams in CloudWatch..."; done ;\
+      INFO "CloudBees CI Log Streams are already in Cloud Watch."
+  fi
 }
 
 test-all () {
