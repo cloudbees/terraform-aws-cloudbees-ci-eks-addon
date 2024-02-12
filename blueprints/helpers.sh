64f7b121fdc8b2e9fdcdd3b3b8bdad710a8f4dd5
Carlos Rodriguez Lopez <it.carlosrodlop@gmail.com>
2024-02-12 12:04:13 +0100

    making wait local

--- blueprints/helpers.sh
+++ blueprints/helpers.sh
@@ -6,8 +6,6 @@ set -euo pipefail
 
 SCRIPTDIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-RETRY_SECONDS=5
-
 INFO () {
   printf "\033[36m[INFO] %s\033[0m\n" "$1"
 }
@@ -83,15 +81,16 @@ tf-destroy () {
 
 probes () {
   local root=$1
+  local wait=5
   eval "$(tf-output "$root" kubeconfig_export)"
   until [ "$(eval "$(tf-output "$root" cbci_oc_pod)" | awk '{ print $3 }' | grep -v STATUS | grep -v -c Running)" == 0 ]; do sleep 10 && echo "Waiting for Operation Center Pod to get ready..."; done ;\
     eval "$(tf-output "$root" cbci_oc_pod)" && INFO "OC Pod is Ready."
-  until eval "$(tf-output "$root" cbci_liveness_probe_int)"; do sleep $RETRY_SECONDS && echo "Waiting for Operation Center Service to pass Health Check from inside the cluster..."; done
+  until eval "$(tf-output "$root" cbci_liveness_probe_int)"; do sleep $wait && echo "Waiting for Operation Center Service to pass Health Check from inside the cluster..."; done
     INFO "Operation Center Service passed Health Check inside the cluster." ;\
-  until eval "$(tf-output "$root" cbci_oc_ing)"; do sleep $RETRY_SECONDS && echo "Waiting for Operation Center Ingress to get ready..."; done ;\
+  until eval "$(tf-output "$root" cbci_oc_ing)"; do sleep $wait && echo "Waiting for Operation Center Ingress to get ready..."; done ;\
     INFO "Operation Center Ingress Ready."
   OC_URL=$(tf-output "$root" cbci_oc_url)
-  until eval "$(tf-output "$root" cbci_liveness_probe_ext)"; do sleep $RETRY_SECONDS && echo "Waiting for Operation Center Service to pass Health Check from outside the clustery..."; done ;\
+  until eval "$(tf-output "$root" cbci_liveness_probe_ext)"; do sleep $wait && echo "Waiting for Operation Center Service to pass Health Check from outside the clustery..."; done ;\
     INFO "Operation Center Service passed Health Check outside the cluster. It is available at $OC_URL."
   if [ "$root" == "01-getting-started" ]; then
     INITIAL_PASS=$(eval "$(tf-output "$root" cbci_initial_admin_password)"); \
@@ -100,9 +99,9 @@ probes () {
   if [ "$root" == "02-at-scale" ]; then
     GENERAL_PASS=$(eval "$(tf-output "$root" cbci_general_password)"); \
     INFO "General Password all users: $GENERAL_PASS."
-    until [ "$(eval "$(tf-output "$root" cbci_controllers_pods)" | awk '{ print $3 }' | grep -v STATUS | grep -v -c Running)" == 0 ]; do sleep $RETRY_SECONDS && echo "Waiting for Controllers Pod to get into Ready State..."; done ;\
+    until [ "$(eval "$(tf-output "$root" cbci_controllers_pods)" | awk '{ print $3 }' | grep -v STATUS | grep -v -c Running)" == 0 ]; do sleep $wait && echo "Waiting for Controllers Pod to get into Ready State..."; done ;\
       eval "$(tf-output "$root" cbci_controllers_pods)" && INFO "All Controllers Pods are Ready."
-    until eval "$(tf-output "$root" cbci_controller_c_hpa)"; do sleep $RETRY_SECONDS && echo "Waiting for Team C HPA to get Ready..."; done ;\
+    until eval "$(tf-output "$root" cbci_controller_c_hpa)"; do sleep $wait && echo "Waiting for Team C HPA to get Ready..."; done ;\
       INFO "Team C HPA is Ready."
     eval "$(tf-output "$root" cbci_oc_export_admin_crumb)" && eval "$(tf-output "$root" cbci_oc_export_admin_api_token)" && \
       if [ -n "$CBCI_ADMIN_TOKEN" ]; then INFO "Admin Token: $CBCI_ADMIN_TOKEN"; else ERROR "Problem while getting Admin Token"; fi
@@ -114,9 +113,9 @@ probes () {
     eval "$(tf-output "$root" velero_backup_schedule_team_a)" && eval "$(tf-output "$root" velero_backup_on_demand_team_a)" > "/tmp/backup.txt" && \
       grep "Backup completed with status: Completed" "/tmp/backup.txt" && \
       INFO "Velero backups are working"
-    until eval "$(tf-output "$root" prometheus_active_targets)" | jq '.data.activeTargets[] | select(.labels.container=="jenkins" or .labels.job=="cjoc") | {job: .labels.job, instance: .labels.instance, status: .health}'; do sleep $RETRY_SECONDS && echo "Waiting for CloudBees CI Prometheus Targets..."; done ;\
+    until eval "$(tf-output "$root" prometheus_active_targets)" | jq '.data.activeTargets[] | select(.labels.container=="jenkins" or .labels.job=="cjoc") | {job: .labels.job, instance: .labels.instance, status: .health}'; do sleep $wait && echo "Waiting for CloudBees CI Prometheus Targets..."; done ;\
       INFO "CloudBees CI Targets are loaded in Prometheus."
-    until eval "$(tf-output "$root" aws_logstreams_fluentbit)" | jq '.[] '; do sleep $RETRY_SECONDS && echo "Waiting for CloudBees CI Log streams in CloudWatch..."; done ;\
+    until eval "$(tf-output "$root" aws_logstreams_fluentbit)" | jq '.[] '; do sleep $wait && echo "Waiting for CloudBees CI Log streams in CloudWatch..."; done ;\
       INFO "CloudBees CI Log Streams are already in Cloud Watch."
   fi
 }
