e0938574720797ff5d5e993087e6ec908b9f4c00
Dragan Bocevski <36728855+DraganBocevski@users.noreply.github.com>
2024-04-01 19:43:27 +0200

    Use online module

--- blueprints/01-getting-started/main.tf
+++ blueprints/01-getting-started/main.tf
@@ -40,7 +40,9 @@ locals {
 # CloudBees CI Add-ons
 
 module "eks_blueprints_addon_cbci" {
-  source = "../../"
+  #source = "../../"
+  source  = "cloudbees/cloudbees-ci-eks-addon/aws"
+  version >= "3.17108.0"
 
   hosted_zone   = var.hosted_zone
   cert_arn      = module.acm.acm_certificate_arn
