ea02d2e3cfab41c10fcb55fcbe22d95c03f73ef4
Carlos Rodriguez Lopez <it.carlosrodlop@gmail.com>
2024-07-10 20:10:54 +0200

    Preparing for realease

--- blueprints/02-at-scale/main.tf
+++ blueprints/02-at-scale/main.tf
@@ -100,7 +100,8 @@ resource "time_static" "epoch" {
 # CloudBees CI Add-ons
 
 module "eks_blueprints_addon_cbci" {
-  source = "../../"
+  source  = "cloudbees/cloudbees-ci-eks-addon/aws"
+  version = ">= 3.18072.0"
 
   depends_on = [module.eks_blueprints_addons]
 
