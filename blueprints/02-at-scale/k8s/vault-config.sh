d3af92a4e5a998991923d29d6cbe426ab81e66d4
Carlos Rodriguez Lopez <it.carlosrodlop@gmail.com>
2024-07-24 14:25:48 +0200

    typo

--- blueprints/02-at-scale/k8s/vault-config.sh
+++ blueprints/02-at-scale/k8s/vault-config.sh
@@ -16,7 +16,7 @@ approle="cbci-oc"
 kubectl exec -it vault-0 -n "$vault_ns" -- vault operator init | tee "$HERE/vault-init.log" || echo "Vault already initialized"
 ## Useal the vault
 for i in {1..3}; do
-  read -r -p "INFO: Enter Unseal Key number $i: [press Enter]" key
+  read -r -p "INFO: Enter Unseal Key number $i [press Enter]: " key
   if [ -z "$key" ]; then
     echo "ERROR: Empty key is not allowed" && exit 1
   fi
