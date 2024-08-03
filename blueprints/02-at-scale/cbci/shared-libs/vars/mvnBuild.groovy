3f458c706705d52d0b539f42fad848a0642a165d
Carlos Rodriguez Lopez <it.carlosrodlop@gmail.com>
2024-08-03 23:38:22 +0200

    redoing the method

--- blueprints/02-at-scale/cbci/shared-libs/vars/mvnBuild.groovy
+++ blueprints/02-at-scale/cbci/shared-libs/vars/mvnBuild.groovy
@@ -1,8 +1,9 @@
+// vars/mvnBuild
 
-def call(boolean skipTests = true) {
-    if (skipTests) {
-        sh 'mvn clean package -DskipTests -Dmaven.repo.local=./maven-repo'
-    } else {
-        sh 'mvn -Dmaven.test.failure.ignore=true install'
-    }
+def call(boolean skipTests=false){
+	if (skipTests) {
+		sh 'mvn clean package -DskipTests -Dmaven.repo.local=./maven-repo'
+	} else {
+		sh 'mvn clean package -Dmaven.repo.local=./maven-repo'
+	}
 }
