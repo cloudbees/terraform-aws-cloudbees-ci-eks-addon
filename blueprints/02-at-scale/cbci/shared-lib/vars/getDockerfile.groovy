9020fd756ffc1454601028c2bb318213915f9ba9
Carlos Rodriguez Lopez <it.carlosrodlop@gmail.com>
2024-08-05 16:38:56 +0200

    Replace dockerfileLocation by fileName for clarity

--- blueprints/02-at-scale/cbci/shared-lib/vars/getDockerfile.groovy
+++ blueprints/02-at-scale/cbci/shared-lib/vars/getDockerfile.groovy
@@ -1,10 +1,10 @@
 // vars/getDockerfile
 
-def call(String dockerfileLocation){
-    if (dockerfileLocation?.trim()) {
-        Object dockerfileContent = libraryResource "dockerfiles/${dockerfileLocation}"
+def call(String fileName){
+    if (fileName?.trim()) {
+        Object dockerfileContent = libraryResource "dockerfiles/${fileName}"
         writeFile file: 'Dockerfile', text: dockerfileContent
     } else {
-        error 'dockerfileLocation is empty. Please provide a valid location.'
+        error 'fileName is empty. Please provide a valid location.'
     }
 }
