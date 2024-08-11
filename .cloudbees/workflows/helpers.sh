d9775a23e0e00bfda43d8fcf06f0118e38c98baa
Carlos Rodriguez Lopez <it.carlosrodlop@gmail.com>
2024-08-11 23:15:44 +0200

    validate help file

--- .cloudbees/workflows/helpers.sh
+++ .cloudbees/workflows/helpers.sh
@@ -11,3 +11,24 @@ s3-create-bucket() {
           --bucket "$bucket_name" \
           --region "$region" || echo "$bucket_name already exists"
 }
+
+kms-delete-alias() {
+    local alias_name=$1
+    local region=$2
+    aws kms delete-alias \
+          --alias-name "$alias_name" \
+          --region "$region" || echo "$alias_name does not exist"
+}
+
+s3-put-object() {
+    local bucket_name=$1
+    local region=$2
+    local key=$3
+    local body=$4
+    aws s3api put-object \
+        --bucket "$bucket_name" \
+        --region "$region" \
+        --key "$key" \
+        --body "$body" || echo "Failed to put object"
+}
+
