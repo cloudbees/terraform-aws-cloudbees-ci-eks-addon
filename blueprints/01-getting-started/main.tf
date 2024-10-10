a917615273f545138004fb5cf0a25906da96b1f3
Carlos Rodriguez Lopez <it.carlosrodlop@gmail.com>
2024-10-10 16:44:06 +0200

    ci:preparing builds

--- blueprints/01-getting-started/main.tf
+++ blueprints/01-getting-started/main.tf
@@ -31,8 +31,9 @@ locals {
 # CloudBees CI Add-on
 
 module "eks_blueprints_addon_cbci" {
-  source  = "cloudbees/cloudbees-ci-eks-addon/aws"
-  version = ">= 3.18306.0"
+  #source  = "cloudbees/cloudbees-ci-eks-addon/aws"
+  #version = ">= 3.18306.0"
+  source     = "../../"
 
   depends_on = [module.eks_blueprints_addons]
 
