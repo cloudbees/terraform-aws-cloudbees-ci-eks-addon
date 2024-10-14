14b3060fad1e06860a7328f5ea041a68d01f26c7
Carlos Rodriguez Lopez <it.carlosrodlop@gmail.com>
2024-10-14 13:02:13 +0200

    Update main.k8s.tf

--- blueprints/02-at-scale/main.k8s.tf
+++ blueprints/02-at-scale/main.k8s.tf
@@ -58,8 +58,8 @@ resource "time_static" "epoch" {
 # CloudBees CI Add-on
 
 module "eks_blueprints_addon_cbci" {
-  # source  = "cloudbees/cloudbees-ci-eks-addon/aws"
-  # version = ">= 3.18072.0"
+  #source  = "cloudbees/cloudbees-ci-eks-addon/aws"
+  #version = ">= 3.18072.0"
   source     = "../../"
   depends_on = [module.eks_blueprints_addons]
 
